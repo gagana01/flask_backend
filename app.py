72561314ec50b624634d54a81d211fde5c293e09
G Gagana Sindhu <86293683+gagana01@users.noreply.github.com>
2023-03-28 08:55:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,58 @@
-# import flask and re
-from flask import Flask, request, render_template
-import re
+import os
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pyshorteners
 
-#Create the object 
 app = Flask(__name__)
 
 
-# Create an END POINT using routes and bind them with a functionality
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class URL(db.Model):
+    __tablename__ = 'URLs'
+    id = db.Column(db.Integer, primary_key = True)
+    user_url = db.Column(db.String(100))
+    short_url = db.Column(db.String(100))
 
 
-@app.route('/', methods=['GET', 'POST'])
-def match_txt():
-    if request.method == 'POST':
-        pattern = request.form['pattern']
-        text = request.form['text']
-        matches = re.findall(pattern,text)
-        count = len(matches)
-        return render_template('search.html', matches=matches, text=text, pattern=pattern, count=count)
-    else:
-        return render_template('search.html')
+    def __init__(self,user_url,short_url):
+        self.user_url = user_url
+        self.short_url= short_url
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/result',methods=['GET',"POST"])
+def result():
+    user_url = request.form['url']
+    str = pyshorteners.Shortener()
+    short_url = str.tinyurl.short(user_url)
+    url = URL(user_url=user_url, short_url = short_url)
+    db.session.add(url)
+    db.session.commit()
+    return render_template('home.html',short_url=short_url)
+
+
+@app.route('/history', methods = ['GET','POST'])
+def history():
+    hist = URL.query.all()
+    return render_template('history.html',hist = hist)
 
 
 
-# Step 4 - Run the app
 if __name__ == '__main__':
     app.run(debug=True)
